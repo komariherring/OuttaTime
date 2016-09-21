e9628c7b47adf60f3e6e300c4596aa979af484a2
Komari Herring <komherr@gmail.com>
2016-09-21 12:27:01 -0400

    Initial Commit

---
+++ OuttaTime/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OuttaTime
+//
+//  Created by Komari Herring on 9/21/16.
+//  Copyright © 2016 The Iron Yard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ OuttaTime/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  OuttaTime
+//
+//  Created by Komari Herring on 9/21/16.
+//  Copyright © 2016 The Iron Yard. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
